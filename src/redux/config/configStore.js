859c6be58a75f9553dad0b578ccfc0b039c96f69
kwonih1020 <kwonih1020@gmail.com>
2022-08-25 18:49:05 +0900

    remove looger

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import jobDetailSlice from "../modules/jobDetailSlice";
 
@@ -13,8 +13,8 @@ const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }).concat(logger),
-  devTools: true,
+    }),
+  // devTools: true,
 });
 
 export default store;
