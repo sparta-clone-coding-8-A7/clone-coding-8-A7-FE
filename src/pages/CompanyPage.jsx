03eab4a8a9c6861bae5ba0f70db99ed65e9fdf36
kwonih1020 <kwonih1020@gmail.com>
2022-08-25 19:59:33 +0900

    create page css

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,15 +1,16 @@
 import CompanyWrite from "../components/companywrite/CompanyWrite";
-import React from 'react'
+import React from "react";
 import Header from "../components/header/Header";
 
 const CompanyPage = () => {
   return (
-    <div className="companypage">
-      내용1
+    <>
       <Header></Header>
-      <CompanyWrite></CompanyWrite>
-    </div>
-  )
-}
+      <div className="companypage">
+        <CompanyWrite></CompanyWrite>
+      </div>
+    </>
+  );
+};
 
-export default CompanyPage
+export default CompanyPage;
